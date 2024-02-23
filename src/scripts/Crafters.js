db31aa4162922a515b0d98f4889b69058ed50489
Zavier Hopson <zo.hopson94@gmail.com>
2024-02-23 12:01:54 -0600

    database access aquired

--- src/scripts/Crafters.js
+++ src/scripts/Crafters.js
@@ -4,14 +4,20 @@
     selected, update transient state.
 */
 
-export const getCrafters = () => {
-  return `
-    <h3>Crafters</h3>
-    <select id="crafter">
-    <option value="" selected disabled hidden>--Select A Type--</option>
-    <option value="0">Whisemaral</option>
-    <option value="1">Flammedra</option>
-    <option value="2">Mireille</option>
-    </select>
-  `;
+export const getCrafters = async () => {
+  const response = await fetch("http://localhost:9001/crafters");
+  const crafters = await response.json();
+
+  let craftHTML = `<h3>Crafters</h3>`;
+  craftHTML += `<select id="crafter">
+  <option value="" selected disabled hidden>--Select A Type--</option>`;
+  const craftStringArray = crafters.map((crafter) => {
+    return ` 
+    <option value="${crafter.id}">${crafter.name}</option>
+    `;
+  });
+
+  craftHTML += craftStringArray.join(" ");
+  craftHTML += `</select>`;
+  return craftHTML;
 };
